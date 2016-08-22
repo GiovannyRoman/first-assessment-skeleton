40ecd5b3949f92d7483091c6c3607455b7a44d61
GiovannyRoman <groman@hawk.iit.edu>
2016-08-22 16:22:23 -0500

    The client has connect, disconnect, echo, broadcast, and users finished but requires to test with mulitple users.
    The connect and disconnect info all other users when someone is connected. Broadcast sends message to all other users.

--- src/main/java/com/cooksys/assessment/server/ClientHandler.java
+++ src/main/java/com/cooksys/assessment/server/ClientHandler.java
@@ -3,20 +3,24 @@ package com.cooksys.assessment.server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.HashSet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.cooksys.assessment.model.Message;
+import com.cooksys.assessment.model.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class ClientHandler implements Runnable {
 	private Logger log = LoggerFactory.getLogger(ClientHandler.class);
-
+	static HashSet<User> users = new HashSet<User>(); // store all users
 	private Socket socket;
+	private String lastCommand;
 
 	public ClientHandler(Socket socket) {
 		super();
@@ -34,26 +38,97 @@ public class ClientHandler implements Runnable {
 				String raw = reader.readLine();
 				Message message = mapper.readValue(raw, Message.class);
 
-				switch (message.getCommand()) {
+				if (message.getCommand().contains("@")) {
+					String buddy = message.getCommand().substring(1);
+					for (User us : users) {
+						if (us.getUsername() == buddy) {
+							String privateMessage = "<"+message.getUsername()+"> (whisper): " + message.getContents();
+							log.info(privateMessage);
+							
+							message.setContents(privateMessage);
+							String pmessage = mapper.writeValueAsString(message);
+							
+							PrintWriter receiver = new PrintWriter(
+									new OutputStreamWriter(us.getSocket().getOutputStream()));
+							receiver.write(pmessage);
+							receiver.flush();
+							break;
+						}
+					}
+				} else {
+					switch (message.getCommand()) {
 					case "connect":
-						log.info("user <{}> connected", message.getUsername());
+
+						String connect = "user <" + message.getUsername() + ">  has connected";
+						users.add(new User(message.getUsername(), socket));
+						message.setContents(connect);
+						log.info("user <{}> has connected", message.getUsername());
+
+						String bmessage = mapper.writeValueAsString(message);
+						for (User everyone : users) {
+							PrintWriter receiver = new PrintWriter(
+									new OutputStreamWriter(everyone.getSocket().getOutputStream()));
+							receiver.write(bmessage);
+							receiver.flush();
+						}
+
 						break;
+						
 					case "disconnect":
-						log.info("user <{}> disconnected", message.getUsername());
+						String disconnect = "user <" + message.getUsername() + ">  has disconnected";
+						users.remove(message.getUsername());
+						message.setContents(disconnect);
+						String dismessage = mapper.writeValueAsString(message);
+
+						log.info(disconnect);
+						for (User everyone : users) {
+							PrintWriter receiver = new PrintWriter(
+									new OutputStreamWriter(everyone.getSocket().getOutputStream()));
+							receiver.write(dismessage);
+							receiver.flush();
+						}
 						this.socket.close();
 						break;
+						
 					case "echo":
 						log.info("user <{}> echoed message <{}>", message.getUsername(), message.getContents());
 						String response = mapper.writeValueAsString(message);
 						writer.write(response);
 						writer.flush();
 						break;
+						
+					case "broadcast":
+						String broadmessage = mapper.writeValueAsString(message);
+						log.info("user <{}> broadcast (all) <{}>", message.getUsername(), message.getContents());
+						for (User everyone : users) {
+							PrintWriter receiver = new PrintWriter(
+									new OutputStreamWriter(everyone.getSocket().getOutputStream()));
+							receiver.write(broadmessage);
+							receiver.flush();
+						}
+						writer.write(broadmessage);
+						writer.flush();
+						break;
+						
+					case "users":
+						String info = "currently connected users: ";
+						for (User user : users) {
+							info += user.getUsername() + " ";
+						}
+						log.info(info);
+						message.setContents(info);
+						String newMessage = mapper.writeValueAsString(message);
+						writer.write(newMessage);
+						writer.flush();
+						break;
+						
+					default:
+					}
+
 				}
 			}
-
 		} catch (IOException e) {
 			log.error("Something went wrong :/", e);
 		}
 	}
-
 }
